63c72ae40bcd2f08da9938656d837be3065aef81
Tryd0g0lik <work80@mail.ru>
2024-09-19 16:41:49 +0700

    [master]: manual a test for the relocation is completed

--- __tests__/tests_inventory_page/test_views.py
+++ __tests__/tests_inventory_page/test_views.py
@@ -1,4 +1,5 @@
-"""Position of the catalog."""
+"""This a test that where we can to make a click and  navigate to
+    the product page."""
 
 import re
 
@@ -21,20 +22,21 @@ async def browsers_inventory():
         yield browser
 
 
+# This is parametrize
 inventory_testdata = [
     (
-        testdata[0][0],
-        testdata[0][1],
-        testdata[0][2],
-        testdata[0][3],
-        ".inventory_item",
-        "text",
-        SECTION_TITLE,
+        testdata[0][0],  # Login
+        testdata[0][1],  # Password
+        "1",  # This id of params, symply
+        testdata[0][3],  # bool. This is no need.
+        ".inventory_item",  # selector for a total/general contain
+        "text",  # This is data type for "inventory_testdata[0][-1]"
+        SECTION_TITLE,  # What expect we for search
     ),
     (
         testdata[0][0],
         testdata[0][1],
-        testdata[0][2],
+        "2",
         testdata[0][3],
         ".inventory_item",
         "role",
@@ -43,7 +45,7 @@ inventory_testdata = [
     (
         testdata[0][0],
         testdata[0][1],
-        testdata[0][2],
+        "3",
         testdata[0][3],
         ".inventory_item",
         "text",
@@ -53,7 +55,7 @@ inventory_testdata = [
     (
         testdata[0][0],
         testdata[0][1],
-        testdata[0][2],
+        "4",
         testdata[0][3],
         ".inventory_item",
         "pattern",
@@ -72,10 +74,9 @@ async def test_combined_inventory(
     ind: str,
     select: str,
     view: str,
-    text: str,
-    excepted: [str, re.Pattern],
+    text: [str, re.Pattern],
+    excepted: bool,
 ):
-
     context = await browsers_inventory.new_context()
     page: Page = await context.new_page()
     if_not_true = await fill_feields(page, login, password)
@@ -88,15 +89,22 @@ async def test_combined_inventory(
     assert "inventory.html" in page.url
 
     # Position 'Sauce Labs Backpack' look up and click
-    await page.screenshot(
-        path=f"./___tests__/tests_inventory_page/pic/screenshot_{ind}_page.png",
-    )
     locators = page.locator(".inventory_item[data-test='inventory-item']")
 
     # title and descrip of position
-    if isinstance(text, str) is str and view == "text":
-        await expect(locators.filter(has_text=text)).to_contain_text(text)
-
+    if isinstance(text, str) and view == "text":
+        await expect(locators.filter(has_text=SECTION_TITLE)).to_contain_text(
+            SECTION_TITLE
+        )
+        ree = locators.get_by_text(text=SECTION_TITLE)
+        await ree.click()
+        await expect(page).to_have_url(
+            re.compile(r"/inventory-item\.html\?id=[0-9]+$"), timeout=7000
+        )
+        await expect(
+            page.locator("button[id='add-to-cart']").get_by_text("Add to cart")
+        ).to_have_text("Add to cart")
+        await page.locator("button[id='add-to-cart']").click()
     # price of position
     elif isinstance(text, re.Pattern) and view == "pattern":
         element = (
@@ -106,30 +114,49 @@ async def test_combined_inventory(
         )
 
         assert await element.count() > 0
+        await page.screenshot(
+            path=f"./___tests__/tests_inventory_page/\
+                pic/screenshot_{ind}_page.png"
+        )
+        # await page.screenshot(
+        #     path=f"./___tests__/tests_inventory_page/pic/screenshot2_{ind}_page.png",
+        # )
+        await locators.filter(has_text=SECTION_TITLE).locator(
+            "button", has_text="Add to cart"
+        ).click()
+        # .get_by_text("Add to cart").click()
 
     # button
     elif view == "role":
+        # Here one section chose and look up to the button
         element = locators.filter(has_text=SECTION_TITLE).get_by_role(
             "button", name=text
         )
         assert await element.count() > 0
+        await element.click()
+        # change the button. If it has selector "#add-to-cart" this means
+        # not True
+        await expect(
+            page.locator("button[id='add-to-cart']").get_by_text(text)
+        ).to_have_count(0)
 
     # Check if it's a regex pattern
-    await page.get_by_text("Sauce Labs Backpack").click()
-    await expect(page).to_have_url(re.compile(r"/inventory-item\.html\?id=[0-9]+$"))
+    try:
+        await expect(page.locator("button[id='add-to-cart']")).to_have_text(
+            "Add to cart"
+        ).not_to_have_count(1)
+        # Look on a cart 1/2
+        locator = page.locator(".shopping_cart_link[data-test='shopping-cart-link']")
+        await expect(locator).to_be_empty()
 
-    # Single position adding into the cart
-    await expect(
-        page.locator("button[id='add-to-cart']").get_by_text("Add to cart")
-    ).to_have_text("Add to cart")
-    # Look on a cart 1/2
-    locator = page.locator(".shopping_cart_link[data-test='shopping-cart-link']")
-    await expect(locator).to_be_empty()
-    await page.locator("button[id='add-to-cart']").click()
-    # Look on a cart 2/2
-    await expect(
-        page.locator(".shopping_cart_link[data-test='shopping-cart-link']")
-    ).not_to_be_empty()
+    except ImportError:
+        pass
+    finally:
 
-    await context.close()
-    await browsers_inventory.close()
+        # Look on a cart 2/2
+        await expect(
+            page.locator(".shopping_cart_link[data-test='shopping-cart-link']")
+        ).not_to_be_empty()
+
+        await context.close()
+        await browsers_inventory.close()
